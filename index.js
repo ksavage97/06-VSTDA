d00ea6b49271d2c4aa58743ab74c5e9c51fc59a3
ksavage97 <ksavage97@gmail.com>
2016-08-12 13:00:49 -0700

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var app = angular.module("app", []);
 
 app.controller("ListController", function($scope) {
 
-
+	//empty array so I have something to actually push/unshift into
     $scope.todos = [];
 
+
+    //populating the select priority options
     $scope.options = [{
         "priority": "A low priority item",
         "value": "green",
@@ -20,15 +22,20 @@ app.controller("ListController", function($scope) {
     }];
 
 
+    //what happens when the "add todo" button is pressed!
     $scope.addNewToDo = function(toDo) {
         var todo = { 
         	"newtask": $scope.newtext, 
         	"value": $scope.selectedOption.value, 
         	"number": $scope.selectedOption.number};
         $scope.todos.unshift(todo);
+        //clears the form
         $scope.newtext=null;
     };
 
+    //I was originally going to place a conditional statement for text input being empty, but maybe some people would like to do that then edit later
+
+    //chop chop
     $scope.removeItem = function(todo, $index) {
         $scope.todos.splice($index, 1);
     };
